96de8121f65cbe1f414e030fc71638701b2ba0a0
ApereLieZ <51800892+ApereLieZ@users.noreply.github.com>
2021-04-07 13:11:19 +0300

    Perfect collisions

---
+++ Tetris-OpenGL/Project1/Project1/Logic.cpp
@@ -0,0 +1,124 @@
+#include "Logic.h"
+#include <iostream>
+#include <random>
+#include <ctime>
+
+namespace TetrisGame {
+
+	void Logic::spawnFigure() 
+	{
+		srand(time(0));
+		typeOfTetramino = rand() % 7;
+		for (int i = 0; i < 4; i++) {
+			a[i].x = figures[typeOfTetramino][i] / 2;
+			a[i].y = figures[typeOfTetramino][i] % 2;
+		}
+	}
+
+	void Logic::downFigure(int** arr, int row, int coll)
+{
+	if (checkCollizionDown(arr, row, coll)) {
+		for (int i = 0; i < 4; i++)
+		{
+			a[i].x += 1;
+		}
+	}
+}
+
+	bool Logic::checkCollizionDown(int** arr, int row, int coll) {
+		for (int i = 0; i < 4; i++)
+			if (a[i].x >= row - 1 || arr[a[i].x + 1][a[i].y] != 0) {
+				addFigureToGameField(arr, row, coll );
+				spawnFigure();
+				checkLine(arr, row, coll);
+				return false;
+			}
+		return 1;
+	}
+
+	void Logic::moveFigure(int dx, int** arr, int row, int coll)
+	{
+		if (checkCollizionLR(arr, row, coll, dx)) {
+			for (int i = 0; i < 4; i++)
+			{
+				a[i].y -= dx;
+			}
+		}
+	}
+
+	bool Logic::checkCollizionLR(int** arr, int row, int coll, int dx) {
+		for (int i = 0; i < 4; i++) {
+			if (a[i].y - dx  < 0 || a[i].y - dx >= coll) {
+				return false;
+			}
+			else if (arr[a[i].x][a[i].y - dx] != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	void Logic::tryToRotate(int** arr, int row, int coll) {
+		Figure p = a[2]; // указываем центр вращения
+		Figure tempA[4];
+		if (typeOfTetramino == 6) return;
+		for (int i = 0; i < 4; i++)
+		{
+			int x = a[i].y - p.y; // y - y0
+			int y = a[i].x - p.x; // x - x0
+			if (p.y - y > coll || p.y + y < 0 || arr[p.x - x][p.y - y] != 0) return;
+			b[i].x = p.x - x;
+			b[i].y = p.y + y;
+		}
+		for (int i = 0; i < 4; i++) {
+			a[i].x = b[i].x;
+			a[i].y = b[i].y;
+		}
+	}
+
+	void Logic::ScoreUp() {
+		this->score += this->deltaScore;
+	}
+	
+	unsigned int Logic::getScore() {
+		return this->score;
+	}
+
+	void Logic::addFigureToGameField(int** arr, int row,int coll) {
+		for (int i = 0; i < 4; i++) {
+			arr[a[i].x][a[i].y] = typeOfTetramino + 1;
+		}
+	}
+
+
+
+	void Logic::replace(int** arr, int row, int coll, int currentRow) {
+		//Clean array
+		for (int i = 0; i < coll; i++) {
+			arr[currentRow][i] = 0;
+		} 
+
+		for (int i = currentRow; i > 1; i--) {
+			for (int j = 0; j < coll; j++) {
+				arr[i][j] = arr[i - 1][j];
+			}
+		}
+
+	}
+
+	void Logic::checkLine(int** arr, int row, int coll) {
+		int temp = 0;
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < coll; j++) {
+				if (arr[i][j] != 0) {
+					temp++; 
+				}
+			}
+			if (temp == coll) {
+				ScoreUp();
+				replace(arr, row,coll, i);
+			}
+			temp = 0;
+		}
+	}
+}
---
+++ Tetris-OpenGL/Project1/Project1/Logic.h
@@ -0,0 +1,53 @@
+#ifndef Logic_h
+#define Logic_h
+
+namespace TetrisGame {
+
+	struct Figure
+	{
+		int x;
+		int y;
+	};
+
+	class Logic
+	{
+		unsigned int score = 0 ;
+		int figures[7][4] =
+		{
+			1,3,5,7, // I
+			2,4,5,7, // Z
+			3,5,4,6, // S
+			3,5,4,7, // T
+			2,3,5,7, // L
+			3,5,7,6, // J
+			2,3,4,5, // O
+		};
+	
+		const int deltaScore = 100;
+	public:
+		Figure a[4]{};
+		Figure b[4]{};
+		Logic() {
+			spawnFigure();
+		}
+		int typeOfTetramino;
+		//Figure* getFigure();
+		void moveFigure(int dx, int** arr, int row, int coll);
+		void downFigure(int** arr, int row, int coll);
+		//void clearFigure(int** arr, int row, int coll);
+		void checkLine(int** arr, int row, int coll);
+		void spawnFigure();
+		void tryToRotate(int** arr, int row, int coll);
+		unsigned int getScore();
+	private:
+		void ScoreUp();
+		void replace(int** arr, int row, int coll, int currentRow);
+		//void drawFigure(int** arr, int row, int coll);
+		void addFigureToGameField(int** arr, int row, int coll);
+		bool checkCollizionDown(int** arr, int row,int coll);
+		bool checkCollizionLR(int** arr, int row, int coll,int dx);
+
+	};
+}
+
+#endif 
