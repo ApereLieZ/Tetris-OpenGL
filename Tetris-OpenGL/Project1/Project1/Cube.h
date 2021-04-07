28b8f9a85cc2e00791606ff5e2d2426b2484501e
ApereLieZ <51800892+ApereLieZ@users.noreply.github.com>
2021-04-07 13:14:34 +0300

    Upload
    Perfect collisions
    Normal color

---
+++ Tetris-OpenGL/Project1/Project1/Cube.h
@@ -0,0 +1,23 @@
+#ifndef Board_h
+#define Board_h
+
+#include "Shape.h"
+
+namespace TetrisGame
+{
+    // Клас, який відповідає за малювання дошки (поля гри)
+    class Cube : public Shape
+    {
+    public:
+        Cube(float xCenter, float yCenter, float zCenter,
+            float xSize, float ySize, float zSize,
+            float* diffColor, float* ambiColor, float* specColor)
+            : Shape(xCenter, yCenter, zCenter,
+                xSize, ySize, zSize,
+                specColor, diffColor, ambiColor) { }
+        virtual void draw();
+    };
+
+}
+#endif
+
